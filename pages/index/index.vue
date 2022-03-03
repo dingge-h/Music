7d25a39748470bdc3c01add8a1adf2ed4d7fffe8
丁雨健 <365409156@qq.com>
2022-03-03 10:54:23 +0800

    '初始化项目'

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		11
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
